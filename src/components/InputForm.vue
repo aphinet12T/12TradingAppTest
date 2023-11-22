c5802d5ecb3765c6e0362d15c95d06dd8793dae7
aphinet12T <aphinet.dam@onetwotrading.co.th>
2023-11-22 17:15:50 +0700

    add input form

---
+++ src/components/InputForm.vue
@@ -0,0 +1,15 @@
+<template>
+    <label for="input" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">{{text}}</label>
+    <input type="text" id="input" :v-model="model"
+        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        :class="size"
+        required>
+</template>
+
+<script setup>
+defineProps({
+  text: String,
+  size: String,
+  model: String,
+});
+</script>
--- src/views/store/StoreAdd.vue
+++ src/views/store/StoreAdd.vue
@@ -1,111 +1,192 @@
 <template>
-    <LayoutSub>
-        <template v-slot>
-            <div class="flex flex-row items-center">
-                <div class="mt-2">
-                    <ButtonBack />
-                </div>
-                <div class="mt-2 ml-2 flex items-center">
-                    <Icon icon="tdesign:store" width="40" />
-                    <span class="ml-2">เพิ่มร้านค้า</span>
-                </div>
+  <LayoutSub>
+    <template v-slot>
+      <div class="flex flex-row items-center">
+        <div class="mt-2">
+          <ButtonBack />
+        </div>
+        <div class="mt-2 ml-2 flex items-center">
+          <Icon icon="tdesign:store" width="40" />
+          <span class="ml-2">เพิ่มร้านค้า</span>
+        </div>
+      </div>
+      <div class="flex flex-row items-center justify-between mt-3">
+        <div class="mt-2 ml-8 flex items-center">
+          <!-- <Icon icon="tdesign:store" width="20"/> -->
+          <span class="ml-2">ข้อมูลร้านค้า</span>
+        </div>
+        <div class="mt-2 mr-8">
+          <p class=" text-gray-500">*ข้อมูลบังคับ</p>
+        </div>
+      </div>
+      <div class="flex justify-center">
+        <div class="bg-white h-96 w-card shadow-md rounded-lg mt-2">
+          <div class=" flex flex-col items-center mt-5">
+            <div class="mb-1">
+              <label for="storeName"
+                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">ชื่อร้านค้า</label>
+              <input type="text" id="storeName" v-model="vStoreName"
+                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-500 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                required>
+              <!-- <InputForm :text="'ชื่อร้านค้า'" :size="'w-500 p-2.5'">
+                    </InputForm> -->
+              <div class="flex justify-end">
+                <label for="storeName" class="block text-sm font-light text-gray-900 dark:text-white">ไม่เกิน 36
+                  ตัวอักษร</label>
+              </div>
             </div>
-            <div class="flex flex-row items-center justify-between mt-3">
-                <div class="mt-2 ml-8 flex items-center">
-                    <Icon icon="tdesign:store" width="20" />
-                    <span class="ml-2">ข้อมูลร้านค้า</span>
-                </div>
-                <div class="mt-2 mr-8">
-                    <p class=" text-gray-500">*ข้อมูลบังคับ</p>
-                </div>
+            <div class="mb-1">
+              <label for="storeTax"
+                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">เลขที่ผู้เสียภาษี</label>
+              <input type="text" id="storeTax"
+                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-500 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                required>
+              <div class="flex justify-end">
+                <label for="storeTax" class="block text-sm font-light text-gray-900 dark:text-white">ไม่เกิน
+                  13 ตัวอักษร</label>
+              </div>
             </div>
-            <div class="flex justify-center">
-                <div class="bg-white h-96 w-card shadow-md rounded-lg mt-2">
-                    <div class=" flex flex-col items-center mt-5">
-                        <div class="mb-1">
-                            <label for="storeName"
-                                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">ชื่อร้านค้า</label>
-                            <input type="text" id="storeName" v-model="vStoreName"
-                                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-[400px] p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                required>
-                            <div class="flex justify-end">
-                                <label for="storeName"
-                                    class="block text-sm font-light text-gray-900 dark:text-white">ไม่เกิน 36
-                                    ตัวอักษร</label>
-                            </div>
-                        </div>
-                        <div class="mb-1">
-                            <label for="storeTax"
-                                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">เลขที่ผู้เสียภาษี</label>
-                            <input type="text" id="storeTax"
-                                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-500 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                required>
-                            <div class="flex justify-end">
-                                <label for="storeTax" class="block text-sm font-light text-gray-900 dark:text-white">ไม่เกิน
-                                    13 ตัวอักษร</label>
-                            </div>
-                        </div>
-                        <div class=" flex flex-row">
-                            <div class="mb-1 mt-">
-                                <label for="storeTel"
-                                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">โทรศัพท์</label>
-                                <input type="number" id="storeTel"
-                                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-220 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                    required>
-                            </div>
-                            <div class="mb-1 ml-5">
-                                <label for="storeRoute"
-                                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">เส้นทาง</label>
-                                <input type="text" id="storeRoute"
-                                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-220 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                    required>
-                            </div>
-                        </div>
-                        <div class="mb-1">
-                            <label for="storeName"
-                                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">ประเภทร้านค้า</label>
-                            <input type="text" id="storeName"
-                                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-500 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                required>
-                        </div>
-                    </div>
-                </div>
+            <div class="flex flex-row">
+              <div class="mb-1 mt-">
+                <label for="storeTel"
+                  class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">โทรศัพท์</label>
+                <input type="number" id="storeTel"
+                  class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-220 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  required>
+              </div>
+              <div class="mb-1 ml-5">
+                <label for="storeRoute"
+                  class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">เส้นทาง</label>
+                <input type="text" id="storeRoute"
+                  class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-220 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  required>
+              </div>
             </div>
-        </template>
-    </LayoutSub>
+            <div class="mb-1">
+              <label for="storeName"
+                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">ประเภทร้านค้า</label>
+              <input type="text" id="storeName"
+                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-500 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                required>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="flex flex-row items-center justify-between mt-3">
+        <div class="mt-2 ml-8 flex items-center">
+          <!-- <Icon icon="tdesign:store" width="20"/> -->
+          <span class="ml-2">ที่อยู่</span>
+        </div>
+      </div>
+      <div class="flex justify-center">
+        <div class="bg-white h-72 w-card shadow-md rounded-lg mt-2">
+          <div class=" flex flex-col items-center mt-5">
+            <div class="mb-1">
+              <label for="storeName" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">ที่อยู่</label>
+              <input type="text" id="storeName"
+                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-500 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                required>
+              <div class="flex justify-end">
+                <label for="storeName" class="block text-sm font-light text-gray-900 dark:text-white">ไม่เกิน 36
+                  ตัวอักษร</label>
+              </div>
+            </div>
+            <div class="flex flex-row">
+              <div class="mb-1 mt-">
+                <label for="storeTel" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">ตำบล</label>
+                <input type="number" id="storeTel"
+                  class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-220 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  required>
+              </div>
+              <div class="mb-1 ml-5">
+                <label for="storeRoute" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">อำเภอ</label>
+                <input type="text" id="storeRoute"
+                  class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-220 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  required>
+              </div>
+            </div>
+            <div class="flex flex-row">
+              <div class="mb-1 mt-">
+                <label for="storeTel" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">จังหวัด</label>
+                <input type="number" id="storeTel"
+                  class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-220 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  required>
+              </div>
+              <div class="mb-1 ml-5">
+                <label for="storeRoute"
+                  class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">รหัสไปรษณีย์</label>
+                <input type="text" id="storeRoute"
+                  class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-220 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  required>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="flex flex-row items-center justify-between mt-3">
+        <div class="mt-2 ml-8 flex items-center">
+          <!-- <Icon icon="tdesign:store" width="20"/> -->
+          <span class="ml-2">ถ่ายภาพ</span>
+        </div>
+      </div>
+      <div class="flex justify-center">
+        <div class="bg-white h-20 w-card shadow-md rounded-lg mt-2">
+          <div class="flex items-center mt-5 relative">
+            <button
+              class="overflow-y-auto absolute left-2.5 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+              type="button" data-drawer-target="drawer-top-example" data-drawer-show="drawer-top-example"
+              data-drawer-placement="top" aria-controls="drawer-top-example">
+              ถ่าย
+            </button>
+            <input type="text"
+              class="block w-500 text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400">
+          </div>
+        </div>
+      </div>
+    </template>
+  </LayoutSub>
 </template>
 
 <script>
 import { Icon } from '@iconify/vue';
 import { ref } from 'vue';
 import LayoutSub from '../LayoutSub.vue';
+import InputForm from '../../components/InputForm.vue';
 import ButtonBack from '../../components/IconBack.vue';
+import { onMounted } from 'vue'
+import { initFlowbite } from 'flowbite'
+
 export default {
-    components: {
-        Icon,
-        LayoutSub,
-        ButtonBack,
-    },
+  components: {
+    Icon,
+    LayoutSub,
+    ButtonBack,
+    InputForm,
+  },
 
-    setup() {
+  setup() {
 
-        const vStoreName = ref('');
-        // const areaInput = ref('');
-        // const provinceInput = ref('');
+    onMounted(() => {
+      initFlowbite();
+    })
 
-        const sendData = () => {
-            // store.setName(nameInput.value);
-            // store.setArea(areaInput.value);
-            // store.setProvince(provinceInput.value);
+    const vStoreName = ref('');
+    // const areaInput = ref('');
+    // const provinceInput = ref('');
 
-            store.sendDataToAPI();
-        };
+    const sendData = () => {
+      // store.setName(nameInput.value);
+      // store.setArea(areaInput.value);
+      // store.setProvince(provinceInput.value);
 
-        return {
-            vStoreName,
-            sendData,
-        };
+      store.sendDataToAPI();
+    };
 
-    }
+    return {
+      vStoreName,
+      sendData,
+    };
+
+  }
 }
 </script>
